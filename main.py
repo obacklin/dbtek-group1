29259d06dab0f84d32dad80fdf22fc7b1eb3db95
victor-021 <30280491+victor-021@users.noreply.github.com>
2021-11-25 11:29:04 +0100

    Add initial files

---
+++ main.py
@@ -0,0 +1,49 @@
+form config import host_name, user_name, user_password, db_name
+import mysql.connector
+from mysql.connector import Error
+
+def create_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+def read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as err:
+        print(f"Error: '{err}'")
+
+connection = create_db_connection(host_name, user_name, user_password, db_name) # Connect to the Database
+
+q1 = """
+SELECT *
+FROM teacher;
+"""
+
+results = read_query(connection, q1)
+
+for result in results:
+  print(result)
